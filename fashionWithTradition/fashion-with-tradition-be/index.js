da7120e7e358c6e4c57ef61e4c93ce567bf442b2
tanjila_shamima <tanjila.akter@bjitgroup.com>
2023-09-16 18:21:10 +0600

    [BE: folder set up

--- fashionWithTradition/fashion-with-tradition-be/index.js
+++ fashionWithTradition/fashion-with-tradition-be/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const connectDatabase = require('./src/config/dbConnect');
 const { serverPort } = require('./src/secret');
+const authRouter = require('./src/router/authRouter');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('api/auth', authRouter);
 
 app.listen(serverPort, async() => {
     console.log('listening on port ' + serverPort);
